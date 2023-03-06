9e5ac6d60b600640f233c2722b163fe42e5024f7
Mohammed <sajidcurious9@gmail.com>
2023-03-07 02:42:01 +0530

    Making home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="text-white">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <h1 className="text-red-600 text-4xl font-bold">NETFLIX</h1>
+      <div>
+        <button className="text-white mr-4">Sign In</button>
+        <button className="bg-red-600 text-white px-6 py-2 rounded-md">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
