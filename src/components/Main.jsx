41d1521661f4539587c36802ae8056df31664f90
Mohammed <sajidcurious9@gmail.com>
2023-03-09 00:46:16 +0530

    Building Main Component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import requests from "../Requests";
+import axios from "axios";
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data);
+    });
+  }, []);
+  console.log(movies);
+
   return <div>Main</div>;
 };
 
