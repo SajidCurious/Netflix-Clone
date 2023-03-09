9624866116274b44591aa160dd9ee7d69f1a14a1
Mohammed <sajidcurious9@gmail.com>
2023-03-10 01:26:09 +0530

    obtimising the apis to use

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,13 +23,16 @@ const Main = () => {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-        <div>
-          <button className="border border-gray-300 text-black bg-gray-300 py-2 px-5">
-            Play
-          </button>
-          <button className="border border-gray-300 text-white  py-2 px-5">
-            Watch Later
-          </button>
+        <div className="absolute w-full top-[30%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div>
+            <button className="border border-gray-300 text-black bg-gray-300 py-2 px-5">
+              Play
+            </button>
+            <button className="border border-gray-300 text-white  py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
         </div>
       </div>
     </div>
