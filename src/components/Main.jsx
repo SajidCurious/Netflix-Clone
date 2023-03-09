2fbd8ff35588e6295a9ed3a147b6f3100b4414e8
Mohammed <sajidcurious9@gmail.com>
2023-03-10 01:00:11 +0530

    designing the main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,11 +17,20 @@ const Main = () => {
   return (
     <div className="w-full h-[550px] text-white">
       <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
         <img
           className="w-full h-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
+        <div>
+          <button className="border border-gray-300 text-black bg-gray-300 py-2 px-5">
+            Play
+          </button>
+          <button className="border border-gray-300 text-white  py-2 px-5">
+            Watch Later
+          </button>
+        </div>
       </div>
     </div>
   );
