e2d7238ff68b4aa487aad7cdf4f3d685d4d202f3
Mohammed <sajidcurious9@gmail.com>
2023-03-12 04:03:35 +0530

    completed row and movie component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,6 +4,7 @@ import { FaHeart, FaRegHeart } from "react-icons/fa";
 import axios from "axios";
 
 const Movie = ({ key, item }) => {
+  const [like, setLike] = useState(false);
   return (
     <>
       <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
@@ -15,6 +16,13 @@ const Movie = ({ key, item }) => {
         <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
           <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
             {item?.title}
+          </p>
+          <p>
+            {like ? (
+              <FaHeart className="absolute top-4 left-4 text-gray-300" />
+            ) : (
+              <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+            )}
           </p>
         </div>
       </div>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,8 +3,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import Movie from "./Movie";
+import { MdChevronLeft } from "react-icons/md";
+import { MdChevronRight } from "react-icons/md";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowId }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,20 +15,36 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
-  console.log(movies);
-
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="flex items-center">
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={40}
+          className="bg-white rounded-full absolute left-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
         <div
-          id={"slider"}
+          id={"slider" + rowId}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          size={40}
+          className="bg-white rounded-full absolute right-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
       </div>
     </>
   );
