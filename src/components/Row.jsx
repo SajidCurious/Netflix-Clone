cc06a8776308b44a17896e0a6ee39919e93796f7
Mohammed <sajidcurious9@gmail.com>
2023-03-12 01:09:30 +0530

    designing the cards in Row component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,8 +2,22 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const Row = () => {
-  return <div>Row</div>;
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  console.log(movies);
+
+  return (
+    <>
+      <h2></h2>
+    </>
+  );
 };
 
 export default Row;
