92de7550c7730ea113769825c230801ad16b58a1
Mohammed <sajidcurious9@gmail.com>
2023-03-12 02:56:30 +0530

    adding slider buttons

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import Movie from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
@@ -16,21 +18,13 @@ const Row = ({ title, fetchURL }) => {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="flex items-center">
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                  {item?.title}
-                </p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
       </div>
