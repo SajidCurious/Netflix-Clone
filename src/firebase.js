ede727c278a9b14bce642fa50e245b9b4a9f1892
Mohammed <sajidcurious9@gmail.com>
2023-03-14 01:38:16 +0530

    adding firebase and authenticating

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFxTF5JYQJNOZ5LnfM7Bp_GQ28lGENVok",
+  authDomain: "netflix-react-clone-f73aa.firebaseapp.com",
+  projectId: "netflix-react-clone-f73aa",
+  storageBucket: "netflix-react-clone-f73aa.appspot.com",
+  messagingSenderId: "798402839781",
+  appId: "1:798402839781:web:be1c4761fd1236cd1ac00c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
