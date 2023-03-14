a5993f2181b2ba63cc310be1d8b0de6778d55c60
Mohammed <sajidcurious9@gmail.com>
2023-03-15 02:51:34 +0530

    building Signup Component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/5523db5a-e2b2-497f-a88b-61f175c3dbad/eb90437e-a876-47c3-a138-ca27772a4d2a/IN-en-20230306-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Signup;
