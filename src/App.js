0e4ba05c0caf9261d49d4232dbe6eb6872f6b5d3
Mohammed <sajidcurious9@gmail.com>
2023-03-15 01:42:04 +0530

    added signin, signout and subscribe functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContextProvider>
     </>
   );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,33 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState({});
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function logIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+  return (
+    <AuthContext.Provider value={{ signUp, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export function UserAuth() {
