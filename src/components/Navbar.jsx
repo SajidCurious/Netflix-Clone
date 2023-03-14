88046ec1e909947bd306fbc4ab41931ffc90cb88
Mohammed <sajidcurious9@gmail.com>
2023-03-15 02:50:41 +0530

    Added links to the Navbar logo, Signin and Signup buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-      <h1 className="text-red-600 text-4xl font-bold">NETFLIX</h1>
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold">NETFLIX</h1>
+      </Link>
       <div>
-        <button className="text-white mr-4">Sign In</button>
-        <button className="bg-red-600 text-white px-6 py-2 rounded-md">
-          Sign Up
-        </button>
+        <Link to="/login">
+          <button className="text-white mr-4">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 text-white px-6 py-2 rounded-md">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
