df815516364103ed863619431c27ffacef2f1d90
Mohammed <sajidcurious9@gmail.com>
2023-03-19 22:34:38 +0530

    a

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -9,6 +9,7 @@ const Signup = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/5523db5a-e2b2-497f-a88b-61f175c3dbad/eb90437e-a876-47c3-a138-ca27772a4d2a/IN-en-20230306-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="/"
         />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
       </div>
     </>
   );
