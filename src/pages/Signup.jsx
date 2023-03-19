e6a3a8f676a10f7bdb55bff9a2564554cc2cdee9
Mohammed <sajidcurious9@gmail.com>
2023-03-19 22:44:56 +0530

    Adding Signup card in Sign up component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -10,6 +10,13 @@ const Signup = () => {
           alt="/"
         />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
