6059c7606571413f400f566bc692440d190d1f07
Mohammed <sajidcurious9@gmail.com>
2023-03-22 02:17:34 +0530

    Making minor changes to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+
+  const handleLogout = async () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
         <h1 className="text-red-600 text-4xl font-bold">NETFLIX</h1>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="text-white mr-4">Sign In</button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 text-white px-6 py-2 rounded-md">
-            Sign Up
-          </button>
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white mr-4">Account</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 text-white px-6 py-2 rounded-md">
+              Log Out
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white mr-4">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 text-white px-6 py-2 rounded-md">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
