96647476f35e19b6080ffb9a1be88fa0ce269fcc
Mohammed <sajidcurious9@gmail.com>
2023-03-21 23:54:49 +0530

    kk

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
-  appId: process.env.REACT_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
