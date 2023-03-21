2d27c329650c969a83ecb97f3da8a0ef3c16cd77
Mohammed <sajidcurious9@gmail.com>
2023-03-22 02:23:34 +0530

    completed editing the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
+      await logOut();
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
@@ -21,11 +24,13 @@ const Navbar = () => {
           <Link to="/account">
             <button className="text-white mr-4">Account</button>
           </Link>
-          <Link to="/signup">
-            <button className="bg-red-600 text-white px-6 py-2 rounded-md">
-              Log Out
-            </button>
-          </Link>
+
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 text-white px-6 py-2 rounded-md"
+          >
+            Log Out
+          </button>
         </div>
       ) : (
         <div>
