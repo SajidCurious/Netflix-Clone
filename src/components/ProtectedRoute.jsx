80598c9099f5ab180eeedfe3dbe21ed2d8172d02
Mohammed <sajidcurious9@gmail.com>
2023-03-23 00:24:18 +0530

    Building the Account Component

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+const ProtectedRoute = () => {
+  return <div>ProtectedRoute</div>;
+};
+
+export default ProtectedRoute;
