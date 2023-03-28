59426a9e93f4df54d796ffd18e273e84860090d0
Mohammed <sajidcurious9@gmail.com>
2023-03-28 15:53:13 +0530

    modified

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SavedShows from "../components/SavedShows";
+// import SavedShows from "../components/SavedShows";
 
 const Account = () => {
   return (
@@ -15,7 +15,7 @@ const Account = () => {
           <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
         </div>
       </div>
-      <SavedShows />
+      {/* <SavedShows /> */}
     </>
   );
 };
